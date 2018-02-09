3c21439150e9d8f45247591db8edc3327d01b22d
cgiger00 <cgiger00@gmail.com>
2018-02-08 23:04:44 -0500

    new files

---
+++ LED_Test/image-gen.h
@@ -0,0 +1,1 @@
+extern const byte pxl[][13][3] PROGMEM;
---
+++ image.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import sys
+
+im = Image.open(sys.argv[1])
+px = im.load()
+
+
+width, height = im.size
+print ('#include <Arduino.h>')
+print ('#include "image-gen.h"')
+
+print ("const byte pxl[][13][3] PROGMEM = { ")
+for x in range(0,23):
+	for y in range(0,13):
+		if (x < width and y < height):
+			color = px[x,y][:3]
+		else:
+			color = (0,0,0)
+		print (str(color).replace('(', '{').replace(')', '}') + ',')
+print ("};")
+
